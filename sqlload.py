10079b2942f524871be845f40067a7f540311718
Wanding Zhou <wzhou1@mdanderson.org>
2014-10-29 16:10:25 -0500

    minor, message omitted

--- sqlload.py
+++ sqlload.py
@@ -14,14 +14,14 @@ def ensure_elem(eclass, ename):
         session.commit()
         return elem
 
-def load_hg19_gencode():
+def load_elem(rv_s, src_s, fn):
 
-    rv = ensure_elem(RefVersion, 'hg19')
-    src = ensure_elem(Source, 'GENCODE')
+    rv = ensure_elem(RefVersion, rv_s)
+    src = ensure_elem(Source, src_s)
     ft_cds = ensure_elem(FeatureType, 'protein_coding')
     
     name2gene = {}
-    trs.parse_gencode_gtf('transvar.download/hg19.gencode.gtf.gz', name2gene)
+    trs.parse_gencode_gtf(fn, name2gene)
     for name, gene in name2gene.iteritems():
         gns = session.query(Gene).filter_by(name=name).all()   # query and find
         if gns:
@@ -47,13 +47,19 @@ def load_hg19_gencode():
             session.add(f)
             session.flush()
 
+            if transcript.cds:
+                transcript.cds.sort()
+                cds_beg = transcript.cds[0][0]
+                cds_end = transcript.cds[-1][1]
+            else:
+                cds_beg = transcript.cds_beg
+                cds_end = transcript.cds_end
             if not transcript.cds: continue
-            transcript.cds.sort()
             t = Transcript(
                 id = f.id,
                 name = transcript.name,
-                cds_beg = transcript.cds[0][0],
-                cds_end = transcript.cds[-1][1],
+                cds_beg = cds_beg,
+                cds_end = cds_end,
                 gene_id = g.id,
                 strand = 1 if transcript.strand == '-' else 0,
             )
@@ -64,58 +70,8 @@ def load_hg19_gencode():
                 session.add(e)
 
     session.commit()
-
-def load_hg19_ucsc():
-
-    rv = ensure_elem(RefVersion, 'hg19')
-    src = ensure_elem(Source, 'UCSC')
-    ft_cds = ensure_elem(FeatureType, 'protein_coding')
-    
-    name2gene = {}
-    trs.parse_ucsc_refgene('transvar.download/hg19.ucsc.txt.gz', name2gene)
-    for name, gene in name2gene.iteritems():
-        gns = session.query(Gene).filter_by(name=name).all()   # query and find
-        if gns:
-            g = gns[0]
-        else:
-            g = Gene(name=name)
-            session.add(g)
-        for transcript in gene.tpts:
-            chms = session.query(Chromosome).filter_by(name=transcript.chrm).all()
-            if chms:
-                chm = chms[0]
-            else:
-                chm = Chromosome(name=transcript.chrm)
-                session.add(chm)
-                session.flush()
-
-            f = Feature(ftype=ft_cds.id,
-                        chrm_id=chm.id,
-                        beg=transcript.beg,
-                        end=transcript.end,
-                        source_id=src.id,
-                        refversion_id=rv.id)
-            session.add(f)
-            session.flush()
-
-            transcript.cds.sort()
-            t = Transcript(
-                id = f.id,
-                name = transcript.name,
-                cds_beg = transcript.cds_beg,
-                cds_end = transcript.cds_end,
-                gene_id = g.id,
-                strand = 1 if transcript.strand == '-' else 0,
-            )
-            session.add(t)
-            session.flush()
-            for exbeg, exend in transcript.exons:
-                e = Exon(tid=t.id, beg=exbeg, end=exend)
-                session.add(e)
-
-    session.commit()
-
 
 if __name__ == '__main__':
-    # load_hg19_gencode()
-    load_hg19_ucsc()
+    # load_elem('hg19', 'UCSC', 'transvar.download/hg19.ucsc.txt.gz')
+    # load_elem('hg19', 'GENCODE', 'transvar.download/hg19.gencode.gtf.gz')
+    load_elem('hg19', 'Ensembl', 'transvar.download/hg19.ensembl.gtf.gz')
