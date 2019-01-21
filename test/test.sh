b93349bee6947f1fc564c4ad0d2c55026a7cf8aa
Wanding Zhou <zhouwanding@gmail.com>
2019-01-21 13:36:15 -0500

    version 2.4.4.20190121

--- test/test.sh
+++ test/test.sh
@@ -9,7 +9,7 @@ python test.py ../docs/source/ outdocs
 mv outdocs/*.rst ../docs/source/
 
 ## 2. upload github
-version="2.4.4.20180121"
+version="2.4.4.20190121"
 modify ../docs/source/conf.py
 modify ../transvar/version.py
 git commit -am "version $version"
